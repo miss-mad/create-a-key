1f1467af92b5c932f0e497175e9c92277370e2b1
miss-mad <mmlh.harris@gmail.com>
2022-07-25 16:13:27 -0400

    prompt loads when page loads

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,164 @@
-// Assignment code here
+// Begins the password generator prompt once page is loaded
+document.addEventListener(
+  "DOMContentLoaded",
+  (event) => {
+    // Creates a variable generateBtn that is set equal to the elements with the #generate ID, which is the red Generate Password button in html
+    var generateBtn = document.querySelector("#generate");
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+    // A way to create variable arrays without having to type each letter separated by ""
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    var lowerCase = "abcdefghijklmnopqrstuvwyxz".split("");
+    var symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
+    var numbers = "0123456789".split("");
 
-function generatePassword() {
-  console.log("Hey! You clicked the button!");
+    // Function expression to prompt user for a series of questions, starting with password length
+    var promptForPasswordLength = function () {
+      var passwordLength = 8;
+      var userInputLength = prompt(
+        "Please choose a password length between 8 and 128."
+      );
 
-// 1. Prompt the user for the password criteria
-//    a. Password length 8 < 128
-//    b. Lowercase, uppercase, numbers, special characters
-//  2. Validate the input
-//     a. At least one character type is selected
-//  3. Generate password based on criteria
+      console.log("USER INPUT: ", userInputLength);
 
- 
-//  4. Display the password to the page
-  return "Generated password goes here."
-};
+      if (userInputLength >= 8 && userInputLength <= 128) {
+        passwordLength = Math.floor(userInputLength);
+      } else {
+        alert(
+          "We insist that password length is between 8 and 128. Please try again."
+        );
+        promptForPasswordLength();
+      }
+      return passwordLength;
+    };
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    // Four function expressions to ask user if they want to include certain criteria for their password
+    var promptForLowerCase = function () {
+      var lowerCase = "";
+      var userInputLowerCase = confirm(
+        "Should your password have LOWERCASE letters?"
+      );
 
-  passwordText.value = password;
+      userInputLowerCase === true
+        ? (lowerCase = userInputLowerCase)
+        : lowerCase !== userInputLowerCase;
 
-}
+      return lowerCase;
+    };
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    var promptForUpperCase = function () {
+      var upperCase = "";
+      var userInputUpperCase = confirm(
+        "Should your password have UPPERCASE letters?"
+      );
+
+      userInputUpperCase === true
+        ? (upperCase = userInputUpperCase)
+        : upperCase !== userInputUpperCase;
+
+      return upperCase;
+    };
+
+    var promptForSymbols = function () {
+      var symbols = "";
+      var userInputSymbols = confirm(
+        "Should your password have SPECIAL CHARACTERS?"
+      );
+
+      userInputSymbols === true
+        ? (symbols = userInputSymbols)
+        : symbols !== userInputSymbols;
+
+      return symbols;
+    };
+
+    var promptForNumbers = function () {
+      var numbers = "";
+      var userInputNumbers = confirm("Should your password have NUMBERS?");
+
+      userInputNumbers === true
+        ? (numbers = userInputNumbers)
+        : numbers !== userInputNumbers;
+
+      return numbers;
+    };
+
+    // Function expression to create variables for choosing random values for all assigned password character option values
+    var getRandomIndex = function (array) {
+      var max = array.length;
+      var min = 0;
+      var randomIndex = Math.floor(Math.random() * (max - min) + min);
+      return randomIndex;
+    };
+
+    // Function expression to join password characters based on the criteria user selected from options above
+    var buildAvailablePasswordValuesArray = function (
+      isLowerCase,
+      isUpperCase,
+      hasSymbols,
+      hasNumbers
+    ) {
+      var availablePasswordValues = [];
+
+      if (isLowerCase) {
+        availablePasswordValues = availablePasswordValues.concat(lowerCase);
+      }
+
+      if (isUpperCase) {
+        availablePasswordValues = availablePasswordValues.concat(upperCase);
+      }
+
+      if (hasSymbols) {
+        availablePasswordValues = availablePasswordValues.concat(symbols);
+      }
+
+      if (hasNumbers) {
+        availablePasswordValues = availablePasswordValues.concat(numbers);
+      }
+
+      return availablePasswordValues;
+    };
+
+    // Function to create the password
+    function generatePassword() {
+      var password = "";
+
+      var passwordLength = promptForPasswordLength();
+
+      var passwordLowerCase = promptForLowerCase();
+      var passwordUpperCase = promptForUpperCase();
+      var passwordSymbols = promptForSymbols();
+      var passwordNumbers = promptForNumbers();
+
+      var availablePasswordValues = buildAvailablePasswordValuesArray(
+        passwordLowerCase,
+        passwordUpperCase,
+        passwordSymbols,
+        passwordNumbers
+      );
+
+      for (var i = 0; i < passwordLength; i++) {
+        password =
+          password +
+          availablePasswordValues[getRandomIndex(availablePasswordValues)];
+      }
+
+      return password;
+    }
+
+    // Write password to the #password input
+    // Call the generatePassword function (above) and then we store the value from that function into this password variable.
+    function writePassword() {
+      var password = generatePassword();
+      // Creates a variable passwordText that is set equal to the elements with the #password ID, which is the card in html (where generated password displays on the screen)
+      var passwordText = document.querySelector("#password");
+      // Takes value from generated password and plugs it into what's displayed on the screen
+      passwordText.value = password;
+    }
+
+    // Add event listener to generate button so that when user clicks the generate button, it will call the writePassword function (above)
+    generateBtn.addEventListener("click", writePassword);
+
+    writePassword();
+  },
+  false
+);
